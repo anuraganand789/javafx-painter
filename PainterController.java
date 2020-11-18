f1605aeebce279661ae788aac313be08a42f4e88
Anurag Anand <anuraganand789@hotmail.com>
2020-11-18 14:56:11 +0530

    added code to make selection of different shape possible

--- PainterController.java
+++ PainterController.java
@@ -1,101 +1,144 @@
-/**
- * Sample Skeleton for 'painter.fxml' Controller Class
- */
 
-import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+
 import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+
 import javafx.scene.input.MouseEvent;
+
 import javafx.scene.layout.Pane;
 
-public class PainterController {
+import javafx.scene.Node;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.StrokeLineCap;
 
-    @FXML // fx:id="blackRadioButton"
-    private RadioButton blackRadioButton; // Value injected by FXMLLoader
+public class PainterController extends PainterControls{
 
-    @FXML // fx:id="colorToggleGroup"
-    private ToggleGroup colorToggleGroup; // Value injected by FXMLLoader
-
-    @FXML // fx:id="redRadioButton"
-    private RadioButton redRadioButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="greenRadioButton"
-    private RadioButton greenRadioButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="blueRadioButton"
-    private RadioButton blueRadioButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="smallRadioButton"
-    private RadioButton smallRadioButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="sizeToggleGroup"
-    private ToggleGroup sizeToggleGroup; // Value injected by FXMLLoader
-
-    @FXML // fx:id="mediumRadioButton"
-    private RadioButton mediumRadioButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="largeRadioButton"
-    private RadioButton largeRadioButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="undoButton"
-    private Button undoButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="clearButton"
-    private Button clearButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="drawingAreaPane"
-    private Pane drawingAreaPane; // Value injected by FXMLLoader
+    private static PenRadius penRadius    = PenRadius.MEDIUM;
+    private static Paint     brushColor   = Color.BLACK;
+    private static PenShape  currentShape = PenShape.CIRCLE;
 
     @FXML
-    void clearButtonPressed(ActionEvent event) {
-
-    }
-
-    @FXML
-    void colorRadioButtonSelected(ActionEvent event) {
-
-    }
-
-    @FXML
-    void drawingAreaMouseDragged(MouseEvent event) {
-
+    void shapeRadioButtonSelected(ActionEvent event){
+        currentShape = (PenShape) shapeToggleGroup.getSelectedToggle().getUserData();
+	System.out.println(currentShape);
     }
 
     @FXML
     void sizeRadioButtonSelected(ActionEvent event) {
+        penRadius  = (PenRadius) sizeToggleGroup.getSelectedToggle().getUserData();
+    }
+    
+    @FXML
+    void colorRadioButtonSelected(ActionEvent event) {
+        brushColor = (Color) colorToggleGroup.getSelectedToggle().getUserData();
+    }
 
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
     }
 
     @FXML
     void undoButtonPressed(ActionEvent event) {
+        final ObservableList<Node> itemsInDrawingAreaPane = drawingAreaPane.getChildren();
+	final int noOfItems = itemsInDrawingAreaPane.size(); 
 
+	if(noOfItems > 0) { itemsInDrawingAreaPane.remove(noOfItems - 1); }
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+        drawingAreaPane.getChildren().add(getShape(event, currentShape));
+    }
+    
+    private static Shape getShape(final MouseEvent event, final PenShape shape){
+
+	final double xAxisPosition = event.getX();
+	final double yAxisPosition = event.getY();
+        final int lineSize      = penRadius.getRadius();
+
+        return switch(shape) {
+	           case RECTANGLE -> getRectangle(
+					       lineSize,
+					       lineSize,
+		                               xAxisPosition,
+					       yAxisPosition,
+					       brushColor
+					       ); 
+		   case CIRCLE -> getCircle(xAxisPosition, yAxisPosition, lineSize, brushColor);
+	       };
+
+    }
+
+    private static Shape getRectangle(final int height, 
+                                      final int width, 
+                                      final double xAxisPosition, 
+				      final double yAxisPosition,
+				      final Paint paint){
+        final Rectangle rectangle = new Rectangle(height, width, paint);
+	rectangle .setStrokeLineCap(StrokeLineCap.ROUND);
+        rectangle .setX(xAxisPosition);
+	rectangle .setY(yAxisPosition);
+
+	return rectangle;
+    }
+
+    private static Shape getCircle(
+                                   final double xAxisPosition,
+				   final double yAxisPosition,
+				   final int    radius,
+				   final Paint  paint
+				   ){
+        return new Circle(xAxisPosition, yAxisPosition, radius, paint);
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert blackRadioButton != null : "fx:id=\"blackRadioButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert colorToggleGroup != null : "fx:id=\"colorToggleGroup\" was not injected: check your FXML file 'painter.fxml'.";
-        assert redRadioButton != null : "fx:id=\"redRadioButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert greenRadioButton != null : "fx:id=\"greenRadioButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert blueRadioButton != null : "fx:id=\"blueRadioButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert smallRadioButton != null : "fx:id=\"smallRadioButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert sizeToggleGroup != null : "fx:id=\"sizeToggleGroup\" was not injected: check your FXML file 'painter.fxml'.";
-        assert mediumRadioButton != null : "fx:id=\"mediumRadioButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert largeRadioButton != null : "fx:id=\"largeRadioButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert undoButton != null : "fx:id=\"undoButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert clearButton != null : "fx:id=\"clearButton\" was not injected: check your FXML file 'painter.fxml'.";
-        assert drawingAreaPane != null : "fx:id=\"drawingAreaPane\" was not injected: check your FXML file 'painter.fxml'.";
 
+        blackRadioButton      .setUserData(Color.BLACK);
+	redRadioButton        .setUserData(Color.RED);
+	greenRadioButton      .setUserData(Color.GREEN);
+	blueRadioButton       .setUserData(Color.BLUE);
+
+	smallRadioButton      .setUserData(PenRadius.SMALL);
+	mediumRadioButton     .setUserData(PenRadius.MEDIUM);
+	largeRadioButton      .setUserData(PenRadius.LARGE);
+        
+	circleRadioButton     .setUserData(PenShape.CIRCLE);
+	rectangleRadioButton  .setUserData(PenShape.RECTANGLE);
+    }
+    
+    private enum PenRadius{
+        SMALL(2), MEDIUM(4), LARGE(6);
+
+	private final int radius;
+
+	private PenRadius(final int radius){ 
+	    this.radius = radius; 
+	}
+
+	public int getRadius() { 
+	    return this.radius; 
+	}
+    }
+
+    private enum PenShape{
+        RECTANGLE, CIRCLE;
     }
 }
+
 
